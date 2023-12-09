fe215a2136bbf21da8e0e7bbe6fedce38dab637d
kaoutar-12 <kmouradi@student.1337.ma>
2023-12-09 20:14:30 +0100

    minimap

--- bonus/ft_draw.c
+++ bonus/ft_draw.c
@@ -6,7 +6,7 @@
 /*   By: kmouradi <kmouradi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 12:36:09 by kmouradi          #+#    #+#             */
-/*   Updated: 2023/12/09 20:11:35 by kmouradi         ###   ########.fr       */
+/*   Updated: 2023/12/09 20:14:29 by kmouradi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -150,11 +150,16 @@ void draw_wall(t_game *game)
     int x;
     int y;
 
-    i = 0;
-    while (i < game->parse->map_h)
+	int start_i = MAX(0, game->player->y - 400 / TILE_SIZE);
+    int end_i = MIN(game->parse->map_h, game->player->y + 400 / TILE_SIZE);
+    int start_j = MAX(0, game->player->x - 400 / TILE_SIZE);
+    int end_j = MIN(game->parse->map_w, game->player->x + 400 / TILE_SIZE);
+
+    i = start_i;
+    while (i < end_i)
     {
-        j = 0;
-        while (j < game->parse->map_w)
+        j = start_j;
+        while (j < end_j)
         {
             x = (WIN_W / 2) - game->player->x + (j * TILE_SIZE);
             y = (WIN_H / 2) - game->player->y + (i * TILE_SIZE);
